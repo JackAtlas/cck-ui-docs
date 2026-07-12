c5228d1593d9f6ee360afaef4d376511f066c267
JackAtlas <jackatlas8864@gmail.com>
2026-07-12 20:13:07 +0800

    chore(deps): use @cck-ui/core

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,8 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
-import CckUI from 'cck-ui'
-import 'cck-ui/dist/index.css'
+import CckUI from '@cck-ui/core'
+import '@cck-ui/core/styles.css'
 
 export default {
   extends: DefaultTheme,
